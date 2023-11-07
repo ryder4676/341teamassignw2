23a03ca5e17cc539d9d78e402148f4cae0eef327
ryder4676 <60828482+ryder4676@users.noreply.github.com>
2023-11-07 14:06:10 -0700

    Team 2 saaignment - finished up to step 7

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger-output.json");
+
 const cors = require('cors');
 const app = express();
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app
   .use(cors())
---
+++ swagger-config.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require("swagger-autogen")();
+
+const outputFile = "./swagger-output.json";
+const endpointsFiles = ["./routes/index.js", "./routes/temple.js"];
+
+const doc = {
+    info:{
+        title: "Temples API",
+        description: "Information about temples",
+
+    },
+    host: "localhost:8080",
+    schemes: ["http"],
+}
+swaggerAutogen(outputFile, endpointsFiles, doc);
